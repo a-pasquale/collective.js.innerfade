f3875a73d17725056a49c6cdb6ca9f12ac28a9db
helmantel <helmantel@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-12-15 21:47:10 +0000

    initial import

---
+++ collective/js/innerfade/browser/folder_innerfade_view.py
@@ -0,0 +1,20 @@
+from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+class FolderInnerfadeView(BrowserView):
+
+    template = ViewPageTemplateFile('folder_innerfade_view.pt')
+
+    def getImages(self):
+
+        return self.context.getFolderContents(contentFilter={'portal_type':'Image'})
+
+    def getLink(self, imageId):
+
+        result = ''
+        links = self.context.getFolderContents(contentFilter={'portal_type':'Link', 'id':imageId+'.link'})
+
+        if links:
+            result = links[0].getRemoteUrl
+
+        return result
---
+++ collective/js/innerfade/browser/javascripts/collective.innerfade.js
@@ -0,0 +1,9 @@
+jq(document).ready(function() {
+    jq('#Innerfade').innerfade({
+        speed: 'slow',
+        timeout: '5000',
+        containerheight: 279,
+        animationtype: 'fade',
+        type: 'sequence'
+    });
+});
